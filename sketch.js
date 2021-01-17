020071e41374d169f398de10cd5ccaaf89bcf885
mikhailarsiwala <rashida.v@gmail.com>
2021-01-17 13:40:40 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var bullet,thickness;
+var speed,weight,wall;
+var deformation;
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  speed=random(233,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet=createSprite(50, 200, 50, 50);
+  bullet.velocityX=speed;
+  bullet.shapeColor=255;
+
+  wall=createSprite(600,200,thickness,height/2);
+  wall.shapeColor=80,80,80;
+
+}
+
+function draw() {
+  background(0);  
+
+  if(wall.x-car.x < (car.width + wall.width/2))
+  {
+    bullet.velocityX=0;
+    var deformation=0.5 * weight * speed* speed/25509;
+    if(deformation>180)
+    {
+      bullet.shapeColor=color(255,0,0);
+
+    }
+    if(deformation<180 && deformation>100)
+    {
+      bullet.shapeColor=color(230,230,0);
+    }
+    if(deformation<100)
+    {
+      bullet.shapeColor=color(0,255,0);
+    }
+  }
+
+
+
+  if(hasCollided(bullet,wall))
+  {
+
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+  }
+
+  if(damage>10)
+  {
+    wall.shapeColor=color(255,0,0)
+  }
+
+
+
+  if(damage<10)
+  {
+    wall.shapeColor=color(0,255,0);
+  }
+  drawSprites();
+}
+
+
+function hasCollided(Lbullet,Lwall)
+{
+   bulletRightEdge=lbullet.x + lbullet.width;
+   wallLeftEdge=lwall.x;
+   if (bulletRightEdge>=wallLeftEdge)
+   {
+     return true
+   }
+   return false;
+}
